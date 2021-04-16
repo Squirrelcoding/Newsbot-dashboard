e9675e7e828125ab3c9c548d3ab9764f946fcc6f
Codingsquirrel <68344468+Squirrelcoding@users.noreply.github.com>
2021-04-15 19:10:48 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Squirrelcoding/Newsbot-dashboard.git
+echo Downloaded repo
+touch key.json
+echo Made key.json, remember to paste your key!
+npm i express firebase-admin lodash vue@2.6.12 vue-server-renderer
+echo Installed dependencies, repo is ready.
